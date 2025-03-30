c13aacb01c569e33ffb75f19e64dd9286624b7b4
Svyatoslav Milkin <svyatoslavmilkin@yandex.ru>
2025-03-30 17:52:34 +0300

    Add Rate limiter configuration

--- src/main/java/by/svyat/resilience4jexample/controller/OrderController.java
+++ src/main/java/by/svyat/resilience4jexample/controller/OrderController.java
@@ -1,9 +1,11 @@
 package by.svyat.resilience4jexample.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,7 +38,20 @@ public class OrderController {
         return ResponseEntity.ok(String.format("Successfully find order with id: [%s]",id));
     }
 
+    @GetMapping("/ratelimiter/{id}")
+    @RateLimiter(name = "ratelimiter_instance", fallbackMethod = "rateLimiterFallback")
+    public ResponseEntity<String> getOrderByIdRateLimiter(@PathVariable("id") int id) {
+        log.debug("Call external service");
+        ResponseEntity<String> response = restTemplate.getForEntity("/response/200", String.class);
+        log.info(response.getBody());
+        return ResponseEntity.ok(String.format("Successfully find order with id: [%s]",id));
+    }
+
     public ResponseEntity<String> fallbackToDb(int id, Exception e) {
         return ResponseEntity.ok(String.format("Successfully find order from db with id %s", id));
+    }
+
+    public ResponseEntity<String> rateLimiterFallback(int id, Exception e) {
+        return new ResponseEntity<>("Does not permit further calls", HttpStatus.TOO_MANY_REQUESTS);
     }
 }
